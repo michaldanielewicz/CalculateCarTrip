138cd6eda321bf6b0670fb510f9087e352717442
Michał Danielewicz <michaldanielewicz94@gmail.com>
2021-02-28 19:09:48 +0100

    update config

--- config.py
+++ config.py
@@ -9,4 +9,19 @@ DATABASE_PASSWORD = 'password'
 DATABASE_URI = 'postgresql://postgres:password@127.0.0.1:5432/cars'
 DATABASE_HOST = '127.0.0.1'
 DATABASE_PORT = '5432'
-    
+
+
+class Config():
+    """Base config."""
+    pass
+
+class ProdConfig():
+    """Production config."""
+    DEBUG = False
+    TESTING = False
+
+class DevConfig():
+    """Development config."""
+    DEBUG = True
+    TESTING = True
+    DATABASE_URI 
